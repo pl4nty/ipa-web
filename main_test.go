72b7ac232a0272c066306a0ab0d806ec043a6cf9
Tom Plant <tom@tplant.com.au>
2024-01-07 13:46:50 +0000

    feat: cache and download entitlements
    Signed-off-by: Tom Plant <tom@tplant.com.au>

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 
 // reserved name, called before all tests
 func TestMain(t *testing.T) {
+	t.Helper()
 	initWithCommand(true, false, "text")
 	err := login()
 	if err != nil {
@@ -15,7 +16,7 @@ func TestMain(t *testing.T) {
 }
 
 func TestSearchBundle(t *testing.T) {
-	data, err := searchBundle("Apple Pages", 15)
+	data, err := searchBundles("Apple Pages", 15)
 	found := false
 	results := []string{}
 	if err == nil {
@@ -34,11 +35,11 @@ func TestSearchBundle(t *testing.T) {
 }
 
 func TestPackageInfo(t *testing.T) {
-	data, err := getPackageInfo("com.apple.Pages")
+	data, err := getBundle("com.apple.Pages")
 	found := false
 	results := []string{}
 	if err == nil {
-		for _, scheme := range data.packageInfo.CFBundleURLTypes {
+		for _, scheme := range data.Information.CFBundleURLTypes {
 			if scheme.CFBundleURLName == "com.apple.iwork.pages-share" {
 				found = true
 				break
