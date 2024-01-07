f80f5328a485731dc9e300f2631c314f450cae4c
Tom Plant <tom@tplant.com.au>
2024-01-07 07:11:08 +0000

    feat: CI tests for search and Info.plist
    Signed-off-by: Tom Plant <tom@tplant.com.au>

---
+++ main_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"testing"
+)
+
+// reserved name, called before all tests
+func TestMain(t *testing.T) {
+	initWithCommand(true, false, "text")
+	login()
+}
+
+func TestSearchBundle(t *testing.T) {
+	data, err := searchBundle("Apple Pages", 15)
+	found := false
+	results := []string{}
+	if err == nil {
+		for _, app := range data.Results {
+			if app.BundleID == "com.apple.Pages" {
+				found = true
+				break
+			}
+			results = append(results, app.BundleID)
+		}
+	}
+
+	if !found {
+		t.Fatalf(`searchBundle("Apple Pages", 15) returned %q, want BundleID match for %#q, %v`, results, "com.apple.Pages", err)
+	}
+}
+
+func TestPackageInfo(t *testing.T) {
+	data, err := getPackageInfo("com.apple.Pages")
+	found := false
+	results := []string{}
+	if err == nil {
+		for _, scheme := range data.packageInfo.CFBundleURLTypes {
+			if scheme.CFBundleURLName == "com.apple.iwork.pages-share" {
+				found = true
+				break
+			}
+			results = append(results, scheme.CFBundleURLName)
+		}
+	}
+
+	if !found {
+		t.Fatalf(`getPackageInfo("com.apple.Pages") returned %q, want CFBundleURLName match for %#q, %v`, results, "com.apple.iwork.pages-share", err)
+	}
+}
